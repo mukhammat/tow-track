ade1b9f85bea791cac3108b4b5124164915a6510
mukhammat <dosnet2200@gmail.com>
2025-05-04 14:23:39 +0300

    u

--- apps/tow-track-server/src/index.ts
+++ apps/tow-track-server/src/index.ts
@@ -1,23 +1,21 @@
 import { Hono } from "hono";
 import router from "./routers"
+import { HTTPException } from "hono/http-exception";
 
 // Start a Hono app
 const app = new Hono();
-
-/**
- * /createOrder
- * /updateOrder
- * /getOrders
- * /createChat
- * /setPartner
- * /changeStatus
- */
 
 app.route("/api", router);
 app.notFound((c) => {
     return c.text('Custom 404 Message', 404)
 });
+
 app.onError((err, c) => {
+    if(err instanceof  HTTPException) {
+        console.error(`${err}`)
+        return c.json({"message": err.message}, err.status);
+    }
+
     console.error(`${err}`)
     return c.json({"message": "Ошибка сервера"}, 500);
 });
