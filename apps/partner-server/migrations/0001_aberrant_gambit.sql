110a19b6aa24f425dc86bf671eec38ed4a35ad5e
mukhammat <dosnet2200@gmail.com>
2025-05-12 18:46:52 +0300

    delete all not use tavles

---
+++ apps/partner-server/migrations/0001_aberrant_gambit.sql
@@ -0,0 +1,6 @@
+DROP TABLE "chats" CASCADE;--> statement-breakpoint
+DROP TABLE "messages" CASCADE;--> statement-breakpoint
+DROP TABLE "offers" CASCADE;--> statement-breakpoint
+DROP TABLE "orders" CASCADE;--> statement-breakpoint
+DROP TYPE "public"."offer_status";--> statement-breakpoint
+DROP TYPE "public"."order_status";
