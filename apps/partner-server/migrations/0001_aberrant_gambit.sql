7a8cc8c9ab80ed73f475f14cf977f49a72cd1854
mukhammat <dosnet2200@gmail.com>
2025-05-19 22:40:50 +0300

    delete partner service

--- apps/partner-server/migrations/0001_aberrant_gambit.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE "chats" CASCADE;--> statement-breakpoint
-DROP TABLE "messages" CASCADE;--> statement-breakpoint
-DROP TABLE "offers" CASCADE;--> statement-breakpoint
-DROP TABLE "orders" CASCADE;--> statement-breakpoint
-DROP TYPE "public"."offer_status";--> statement-breakpoint
-DROP TYPE "public"."order_status";
